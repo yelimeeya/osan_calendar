750e40f772c2baa8ce201812bd98bafe568a0fd9
yeye <yelimee1004@gmail.com>
2025-09-21 01:14:55 +0900

    api 분리 안함

--- src/main/java/com/osan/calendar/main/controller/AccommodationController.java
+++ src/main/java/com/osan/calendar/main/controller/AccommodationController.java
@@ -2,6 +2,7 @@ package com.osan.calendar.main.controller;
 
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -16,17 +17,10 @@ public class AccommodationController {
 
     // 숙소 현황
     @GetMapping("/accommodation")
-    public String accommodation() {
-        return "/main/accommodation";
-    }
-
-    @GetMapping("/api/accommodation/data")
-    public ResponseEntity<String> getAccommodationData() throws Exception {
+    public String accommodation(Model model) throws Exception {
         ClassPathResource resource = new ClassPathResource("mock/accommodation_mock_data.json");
         String jsonData = StreamUtils.copyToString(resource.getInputStream(), StandardCharsets.UTF_8);
-System.out.println(jsonData);
-        return ResponseEntity.ok()
-                .header("Content-Type", "application/json")
-                .body(jsonData);
+        model.addAttribute("accommodationData", jsonData);
+        return "/main/accommodation";
     }
 }
