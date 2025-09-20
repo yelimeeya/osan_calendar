1d77645ea3a6cd9e5c71b28f1e8e4b97555b8391
yeye <yelimee1004@gmail.com>
2025-09-21 00:49:15 +0900

    mock data json 적용

--- src/main/java/com/osan/calendar/main/controller/AccommodationController.java
+++ src/main/java/com/osan/calendar/main/controller/AccommodationController.java
@@ -1,18 +1,32 @@
 package com.osan.calendar.main.controller;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
+
+import java.nio.charset.StandardCharsets;
 
 @Controller
 @RequestMapping("/main")
 public class AccommodationController {
 
     // 숙소 현황
-    @RequestMapping(value = "/accommodation", method={RequestMethod.GET,RequestMethod.POST})
-    public String accommodation(HttpServletRequest request) {
+    @GetMapping("/accommodation")
+    public String accommodation() {
         return "/main/accommodation";
+    }
+
+    @GetMapping("/api/accommodation/data")
+    public ResponseEntity<String> getAccommodationData() throws Exception {
+        ClassPathResource resource = new ClassPathResource("mock/accommodation_mock_data.json");
+        String jsonData = StreamUtils.copyToString(resource.getInputStream(), StandardCharsets.UTF_8);
+System.out.println(jsonData);
+        return ResponseEntity.ok()
+                .header("Content-Type", "application/json")
+                .body(jsonData);
     }
 }
